123fff3ba8a20f3b9efacdd96d087de3df0352cc
hendisantika <hendisantika@yahoo.co.id>
2023-02-25 20:49:20 +0700

    #10 - Add RecordNotFoundException ✏️ 📗 :octocat:

---
+++ src/main/java/com/hendisantika/exception/RecordNotFoundException.java
@@ -0,0 +1,28 @@
+package com.hendisantika.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-product-categories-search-pagination
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/25/23
+ * Time: 20:49
+ * To change this template use File | Settings | File Templates.
+ */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class RecordNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public RecordNotFoundException(String message) {
+        super(message);
+    }
+
+    public RecordNotFoundException(String message, Throwable t) {
+        super(message, t);
+    }
+}
