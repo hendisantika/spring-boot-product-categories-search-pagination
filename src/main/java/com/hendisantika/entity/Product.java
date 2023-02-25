99ddee5fbd7449b500f3d1a33b5be172e796cc6d
hendisantika <hendisantika@yahoo.co.id>
2023-02-25 20:46:31 +0700

    #7 - Add Product Entity ✏️ 📗 :octocat:

---
+++ src/main/java/com/hendisantika/entity/Product.java
@@ -0,0 +1,49 @@
+package com.hendisantika.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-product-categories-search-pagination
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/25/23
+ * Time: 20:46
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "product")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
