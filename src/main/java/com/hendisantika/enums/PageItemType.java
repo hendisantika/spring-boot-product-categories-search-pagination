1e9888aec9df1bf34299dc0e6237348636b2a1d2
hendisantika <hendisantika@yahoo.co.id>
2023-02-25 20:48:53 +0700

    #9 - Add PageItemType Enum ✏️ 📗 :octocat:

---
+++ src/main/java/com/hendisantika/enums/PageItemType.java
@@ -0,0 +1,15 @@
+package com.hendisantika.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-product-categories-search-pagination
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/25/23
+ * Time: 20:48
+ * To change this template use File | Settings | File Templates.
+ */
+public enum PageItemType {
+    DOTS, PAGE
+}
