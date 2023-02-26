5ee06d7b8069aaf3a0b2da3c7aabcc90f2cc25aa
hendisantika <hendisantika@yahoo.co.id>
2023-02-26 17:59:40 +0700

    #39 - Add init category data table script ✏️📗:octocat:

---
+++ src/main/resources/db/migration/V3_26022023_1750__Init_Category_Data.sql
@@ -0,0 +1,9 @@
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` (`id`, `is_active`, `name`) VALUES (1,_binary '\0','ATK'),(2,_binary '\0','SEMBAKO');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
