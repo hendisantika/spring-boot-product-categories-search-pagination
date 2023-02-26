05f4248ef56ef635e0d08e90d186e3cfb6c2f6a2
hendisantika <hendisantika@yahoo.co.id>
2023-02-26 16:56:04 +0700

    #17 - Add Paging ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/bean/Paging.java
@@ -0,0 +1,32 @@
+package com.hendisantika.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-product-categories-search-pagination
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2/26/23
+ * Time: 16:55
+ * To change this template use File | Settings | File Templates.
+ */
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Paging {
+
+    private static final int PAGINATION_STEP = 3;
+
+    private boolean nextEnabled;
+    private boolean prevEnabled;
+    private int pageSize;
+    private int pageNumber;
+}
