1c0be0ce03cbed807304947cafd1892674ac5e09
hendisantika <hendisantika@yahoo.co.id>
2023-02-27 17:14:19 +0700

    #47 - Add customersPage in CustomerController ✏️📗:whale:

--- src/main/java/com/hendisantika/controller/CustomerController.java
+++ src/main/java/com/hendisantika/controller/CustomerController.java
@@ -1,8 +1,11 @@
 package com.hendisantika.controller;
 
 import com.hendisantika.repository.CustomerRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +21,22 @@ import org.springframework.stereotype.Controller;
 @RequiredArgsConstructor
 public class CustomerController {
     private final CustomerRepository customerRepository;
+
+    @GetMapping("/customers")
+    public String customersPage(HttpServletRequest request, Model model) {
+
+        int page = 0; //default page number is 0 (yes it is weird)
+        int size = 10; //default page size is 10
+
+        if (request.getParameter("page") != null && !request.getParameter("page").isEmpty()) {
+            page = Integer.parseInt(request.getParameter("page")) - 1;
+        }
+
+        if (request.getParameter("size") != null && !request.getParameter("size").isEmpty()) {
+            size = Integer.parseInt(request.getParameter("size"));
+        }
+
+        model.addAttribute("customers", customerRepository.findAll(PageRequest.of(page, size)));
+        return "customers";
+    }
 }
